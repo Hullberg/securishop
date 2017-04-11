3ac72909ecb5191ff8d1185f31fd634e4841266b
Hullberg <johan.hullberg@gmail.com>
2017-04-11 10:23:53 +0200

    got the the items through to checkout

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php
-/*if (isset($_SESSION['username'])) {
-	$_COOKIE['username'] = $_SESSION['username'];
-}*/
-/*if (isset($_COOKIE['username'])) {
-		header('Location: welcome.php');
-		exit;
-}*/
-
-// Checkout
-/*if (isset($_POST['checkoutpurchase'])) {
-	header('Location: checkout.php');
-	exit;
-}*/
-
-session_start();
+if (!isset($_SESSION)) {
+	session_start();
+}
 $name = $_POST['username'];
 $pass = $_POST['password'];
 $time = time();
@@ -161,7 +149,7 @@ if (isset($_POST['clearcart'])) {
 						<!-- dropdown thingy for info -->
 						Hashing! &emsp;
 						<div class="dropdown">
-						  <img src="http://pccweb.ca/standrewsdresden/files/2016/03/Question-mark.png" style="width:10px;height:10px">
+						  <img src="https://image.flaticon.com/icons/png/512/8/8235.png" style="width:15px;height:15px">
 						  <div class="dropdown-content">
 						    <p>Hashing is when you apply an algorithm to a string a certain amount of times, and cannot be undone.</p>
 						  </div>
@@ -192,7 +180,7 @@ if (isset($_POST['clearcart'])) {
 								$total_sum = $total_sum + $prod[price]*$prod[amount];
 								$tempcount = $tempcount + $prod[amount];
 							}
-							// count($_SESSION['cart_products'])
+							$_SESSION['total_sum'] = $total_sum;
 							echo $tempcount.' items - '.$total_sum.' SEK';
 							?>
 							<b class="caret"></b>
@@ -203,15 +191,15 @@ if (isset($_POST['clearcart'])) {
 							foreach($_SESSION['cart_products'] as $prod) {
 								echo "<p>".$prod[prod_name]." x ".$prod[amount]." <span class='pull-right'>".$prod[price]."</span></p>";
 							}
-							//echo "<a class='btn btn-primary'>Checkout</a>";
-							echo "<form method='post' action='checkout.php'>";
-							echo "<input type='hidden' name='checkoutpurchase' value='".$_SESSION['cart_products']."'></input>";
-							echo "<button class='btn btn-success'>Checkout</button>";
-							echo "</form>";
-							echo "<form method='post' action='index.php'>";
+							echo "<form method='post' action='index.php' style='display:inline-block;'>";
 							echo "<input type='hidden' name='clearcart'></input>";
 							echo "<button class='btn btn-success'>Clear Cart</button>";
 							echo "</form>";
+							echo "<form method='post' action='checkout.php' style='display:inline-block;margin-left:3em'>";
+							echo "<input type='hidden' name='checkoutpurchase' value='".$_SESSION['cart_products']."'></input>";
+							echo "<button class='btn btn-success'>Checkout</button>";
+							echo "</form>";
+							
 
 							?>
 						</div>
@@ -309,23 +297,22 @@ if (isset($_POST['clearcart'])) {
 								}
 							}
 							foreach ($db->query($sql) as $row) {
-								print "<li class='span3'>";
-								print "<div class='thumbnail'>";
-								print "<img src='" . $row['imgurl'] . "' alt='' style='height:200px;width:100px'>";
-								print "<div class='caption'>";
-								//print "<h1>" . $row['id'] . "</h1>";
-								print "<h4>" . $row['name'] . "</h4>";
-								print "<p>" . $row['price'] . "</p>";
-								print "<form method='POST' action='index.php'>";
-								print "<input type='hidden' name='product_id' value='".$row['id']."' />";
-								print "<input type='hidden' name='product_name' value='".$row['name']."' />";
-								print "<input type='hidden' name='product_price' value='".$row['price']."' />";
-								print "<input type='hidden' name='type' value='add_product' />";
-								print "<button class='btn btn-success'>Add to Cart</button>";
-								print "</form>";
-								print "</div>";
-								print "</div>";
-								print "</li>";
+								echo "<li class='span3'>";
+								echo "<div class='thumbnail'>";
+								echo "<img src='" . $row['imgurl'] . "' alt='' style='height:200px;width:100px'>";
+								echo "<div class='caption'>";
+								echo "<h4>" . $row['name'] . "</h4>";
+								echo "<p>" . $row['price'] . "</p>";
+								echo "<form method='POST' action='index.php'>";
+								echo "<input type='hidden' name='product_id' value='".$row['id']."' />";
+								echo "<input type='hidden' name='product_name' value='".$row['name']."' />";
+								echo "<input type='hidden' name='product_price' value='".$row['price']."' />";
+								echo "<input type='hidden' name='type' value='add_product' />";
+								echo "<button class='btn btn-success'>Add to Cart</button>";
+								echo "</form>";
+								echo "</div>";
+								echo "</div>";
+								echo "</li>";
 							}
 						}
 						catch (PDOException $e) {
@@ -337,50 +324,12 @@ if (isset($_POST['clearcart'])) {
 		</div>
 	</div>
 	
-	<hr />
+	
 	<!--
+	<hr />
 	<footer id="footer" class="vspace20">
 		<div class="container">
-			<div class="row">
-				<div class="span4">
-					<h4>Info</h4>
-					<ul class="nav nav-stacked">
-						<li><a href="#">Sell Conditions</a>
-						<li><a href="#">Shipping Costs</a>
-						<li><a href="#">Shipping Conditions</a>
-						<li><a href="#">Returns</a>
-						<li><a href="#">About Us</a>
-					</ul>
-				</div> 
-
-				<div class="span4">
-					<h4>Location and Contacts</h4>
-					<p><i class="icon-map-marker"></i>Uppsala</p>
-					<p><i class="icon-envelope"></i>&nbsp;Email: info@mydomain.com</p>
-					<p><i class="icon-globe"></i>&nbsp;Web: http://www.mydomain.com</p>
-				</div>
-
-				<div class="span4">
-					<h4>Newsletter</h4>
-					<p>Write you email to subscribe to our Newsletter service. Thanks!</p>
-					<form class="form-newsletter">
-						<div class="input-append">
-							<input type="email" class="span2" placeholder="your email">
-							<button type="submit" class="btn">Subscribe</button>
-						</div>
-					</form>
-				</div>
-
-			</div>
-
-			<div class="row">
-				<div class="span6">
-					<p>&copy; Copyright 2012.&nbsp;<a href="#">Privacy</a>&nbsp;&amp;&nbsp;<a href="#">Terms and Conditions</a></p>
-				</div>
-				<div class="span6">
-					<a class="pull-right" href="http://www.responsivewebmobile.com" target="_blank">credits by Responsive Web Mobile</a>
-				</div>
-			</div>
+			Cred: rwm-ecommerce for bootstrap template 
 		</div>
 	</footer>
 	-->
--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
 
 if (isset($_COOKIE['username'])) {
 	setcookie('username', "", time()-1);
+	unset($_SESSION['username']);
 }
 header('Location: index.php');
 exit;
